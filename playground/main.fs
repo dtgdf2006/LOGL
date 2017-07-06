537e622642cd8c715b34a259198885ebf45f560c
dtgdf2006 <dtgdf2006@7cc03f52-3392-4d70-b1b8-247bd774517a>
2017-07-06 11:32:19 +0000

    - update

--- playground/main.fs
+++ playground/main.fs
@@ -2,12 +2,26 @@
 out vec4 FragColor;
 
 in vec2 st;
+in vec3 normal;
+in vec3 fragPos;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform float switchTexture;
+uniform vec3 lightPos;
+uniform vec3 cameraPos;
 
 void main()
 {
-   FragColor = mix(texture(texture1, st), texture(texture2, st), switchTexture);
+	float ambientStrength = 0.1f;
+	vec3 lightColor = vec3(1.0, 1.0, 1.0);
+	//FragColor = mix(texture(texture1, st), texture(texture2, st), switchTexture);
+	vec3 lightDir = normalize(lightPos - fragPos);
+	vec3 norm = normalize(normal);
+	float diffStrength = max(dot(lightDir, norm), 0.0f);
+	vec3 diff = lightColor * diffStrength;
+	vec3 viewDir = normalize(cameraPos - fragPos);
+	float spec = pow(max(dot(reflect(-lightDir, norm), viewDir), 0.0), 32);
+	vec3 specular = 0.6 * spec * lightColor;
+	FragColor.rgb = vec3(1.0f, 0.5f, 0.31f) * (lightColor * ambientStrength + diff + specular);
 }
--- playground/main.vs
+++ playground/main.vs
@@ -1,13 +1,20 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 texCoord;
+layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aNormal;
 
 out vec2 st;
+out vec3 normal;
+out vec3 fragPos;
 
 uniform mat4 MVP;
+uniform mat3 normalModel;
+uniform mat3 model;
 
 void main()
 {
-   gl_Position = MVP * vec4(aPos.xyz, 1.0);
-   st = texCoord;
+	gl_Position = MVP * vec4(aPos, 1.0);
+	st = aTexCoord;
+	normal = normalModel * aNormal;
+	fragPos = model * aPos;
 }
