47ceea6cc8af6f241969b81eefbd99384b811959
dtgdf2006 <dtgdf2006@7cc03f52-3392-4d70-b1b8-247bd774517a>
2017-07-10 08:50:22 +0000

    - update

--- playground/main.fs
+++ playground/main.fs
@@ -5,23 +5,38 @@ in vec2 st;
 in vec3 normal;
 in vec3 fragPos;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform float switchTexture;
-uniform vec3 lightPos;
+
+struct Light {
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+struct Material {
+	sampler2D diffuse;
+	sampler2D specular;
+	
+	float shininess;
+};
+
+
+uniform Light light;
+uniform Material material;
 uniform vec3 cameraPos;
 
 void main()
 {
-	float ambientStrength = 0.1f;
-	vec3 lightColor = vec3(1.0, 1.0, 1.0);
-	//FragColor = mix(texture(texture1, st), texture(texture2, st), switchTexture);
-	vec3 lightDir = normalize(lightPos - fragPos);
+	vec3 ambient = light.ambient * texture(material.diffuse, st).rgb;
+	vec3 lightDir = normalize(light.position - fragPos);
 	vec3 norm = normalize(normal);
 	float diffStrength = max(dot(lightDir, norm), 0.0f);
-	vec3 diff = lightColor * diffStrength;
+	vec3 diffuse = light.diffuse * diffStrength * texture(material.diffuse, st).rgb;
 	vec3 viewDir = normalize(cameraPos - fragPos);
-	float spec = pow(max(dot(reflect(-lightDir, norm), viewDir), 0.0), 32);
-	vec3 specular = 0.6 * spec * lightColor;
-	FragColor.rgb = vec3(1.0f, 0.5f, 0.31f) * (lightColor * ambientStrength + diff + specular);
+	float spec = pow(max(dot(reflect(-lightDir, norm), viewDir), 0.0), material.shininess);
+	vec3 specular = spec * light.specular * texture(material.specular, st).rgb;
+	FragColor.rgb = ambient + diffuse + specular;
+	
+	//FragColor = texture(material.diffuse, st);
 }
