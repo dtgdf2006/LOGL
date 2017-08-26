126824ef9c7e7d787c9d314cebd7e198d84061b9
dtgdf2006 <dtgdf2006@7cc03f52-3392-4d70-b1b8-247bd774517a>
2017-08-26 09:51:28 +0000

    - update

--- trunk/playground/main.cpp
+++ trunk/playground/main.cpp
@@ -81,6 +81,9 @@ int main()
 	glStencilOp(GL_KEEP, GL_REPLACE, GL_REPLACE);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_FRONT);
+	//glFrontFace(GL_CW);
 
 	// build and compile shaders
 	// -------------------------
